588cee853798b2b019e406641f7d387d76755823
R1stAMOR <mortazap53@gmail.com>
2026-02-07 11:43:22 +0430

    coding the below parts:
    making the screen,
    the field,
    sitting the start position,
    sitting the steps,
    sitting the finishing line,
    moving turtle and its logic.

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(player.move, "space")
+
 game_on = True
 while game_on:
     time.sleep(0.1)
--- player.py
+++ player.py
@@ -1,6 +1,16 @@
+from turtle import Turtle
+
 Starting_position = (0, -280)
-Move_distance = 10
+Move_distance = 20
 Finishing_line = 280
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.setheading(90)
+        self.penup()
+        self.goto(Starting_position)
+    def move(self):
+        if self.ycor() < Finishing_line:
+            self.forward(Move_distance)
